962ceffe16c5cd8510cacfbb2d5c60bc870e7ab9
Eduardo Diogo <educdiogo@hotmail.com>
2024-11-16 17:06:54 -0400

    feat: styling of the layout

--- src/@types/styles.d.ts
+++ src/@types/styles.d.ts
@@ -1,8 +1,9 @@
 import 'styled-components'
-import { defaultTheme } from './styles/themes/default'
+import { defaultTheme } from '../styles/themes/default'
 
 type ThemeType = typeof defaultTheme
 
 declare module 'styled-components' {
-	export type DefaultTheme = ThemeType
+	// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+	export interface DefaultTheme extends ThemeType { }
 }
