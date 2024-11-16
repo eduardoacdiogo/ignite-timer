8eb8096fac84730fdf94c4fe57b59b431fa6b4b1
Eduardo Diogo <educdiogo@hotmail.com>
2024-11-15 23:20:05 -0400

    feat: created routes and layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+
+import { Router } from './Router'
+
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyled } from './styles/global'
+
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello Word!</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyled />
     </ThemeProvider>
   )
