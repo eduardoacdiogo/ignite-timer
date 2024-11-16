36b30de446e422a06fbfa96973daedec6d73b084
Eduardo Diogo <educdiogo@hotmail.com>
2024-11-15 22:15:04 -0400

    feature: config styled components and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyled } from './styles/global'
+
 export function App() {
   return (
-    <h1>Hello Word!</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello Word!</h1>
+      <GlobalStyled />
+    </ThemeProvider>
   )
 }
