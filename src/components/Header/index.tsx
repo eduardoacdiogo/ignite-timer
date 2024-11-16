abf0d81c8743f24e0e9a4b1efb3f08c256eaddad
Eduardo Diogo <educdiogo@hotmail.com>
2024-11-16 17:57:07 -0400

    feat: Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,24 @@
+import { Scroll, Timer } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
+
+import logoIgnite from '../../assets/logo-ignite.svg'
+
+import { HeaderContainer } from './styles'
+
 export function Header() {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <HeaderContainer>
+      <span>
+        <img src={logoIgnite} alt="" />
+      </span>
+      <nav>
+        <NavLink to="/" title='timer'>
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title='history'>
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
   )
 }
