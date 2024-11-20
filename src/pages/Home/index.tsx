1ffc65cedb832a108dedf6ee092fb2feec6317be
Eduardo Diogo <educdiogo@hotmail.com>
2024-11-20 12:00:10 -0400

    feat: home page structure

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,47 @@
+import { Play } from 'phosphor-react'
+
+import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from './styles'
+
 export function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">I'm gonna work on</label>
+          <TaskInput id="task" list='task-suggestions' placeholder='Give a name to your project' />
+
+          <datalist id='task-suggestions'>
+            <option value='Front-end project' />
+            <option value='Back-end project' />
+            <option value='Mobile project' />
+          </datalist>
+
+          <label htmlFor="task">for</label>
+          <MinutesAmountInput
+            type='number'
+            id="minutesAmount"
+            placeholder='00'
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutes.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton type='submit'>
+          <Play />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
   )
 }
