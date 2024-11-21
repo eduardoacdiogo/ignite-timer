79bbc8c3db9fdb9db8d7ad2e9419232f0b2f9154
Eduardo Diogo <educdiogo@hotmail.com>
2024-11-20 22:12:44 -0400

    feat: history page structure

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,7 +1,36 @@
+import { HistoryContainer, HistoryList, Status } from './styles';
+
 export function History() {
   return (
-    <div>
-      <h1>History</h1>
-    </div>
+    <HistoryContainer>
+      <h1>My history</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Duration</th>
+              <th>Started at</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Task 1</td>
+              <td>1h 30m</td>
+              <td>10:00</td>
+              <td><Status statusColor='green'>Completed</Status></td>
+            </tr>
+            <tr>
+              <td>Task 1</td>
+              <td>1h 30m</td>
+              <td>10:00</td>
+              <td><Status statusColor='green'>Completed</Status></td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
   )
 }
